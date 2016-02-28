e200030647b913d43844cc6248916f502ce27eca
Jon DeVree <nuxi@vault24.org>
2016-02-27 21:10:12 -0500

    raptor

---
+++ raptor.js
@@ -0,0 +1,12 @@
+$(window).load(function() {
+    $('.button').raptorize({
+        'enterOn' : 'konami-code'
+    });
+});
+
+$(window).load(function() {
+    $('.button').raptorize({
+        'enterOn' : 'timer',
+        'delayTime' : 5000
+    });
+});
